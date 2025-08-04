7a0400db66ec350150094ab9d212c9c17a8752e1
ashrafmakandar <37826738+ashrafmakandar@users.noreply.github.com>
2025-08-04 16:46:22 +0530

    categroies added in setting page

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
 
 const settings = () => {
+
+  const NEWS_CATEGORIES = [
+  'business',
+  'entertainment',
+  'general',
+  'health',
+  'science',
+  'sports',
+  'technology',
+];
+ const [selectedCategoRY, setSelectedCategory] = useState<string>("");
+
+
   return (
+    
     <View>
       <Text>settings</Text>
     </View>
